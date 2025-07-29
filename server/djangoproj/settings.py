dd47647047a6cfe4a4c7402514a77e2cad1d6bbd
James Blanchard <jblanchardjr@gmail.com>
2025-07-28 23:04:16 -0400

    Added superuser

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', 'https://jblanchardjr-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
-CSRF_TRUSTED_ORIGINS = ['https://jblanchardjr-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/']
+CSRF_TRUSTED_ORIGINS = [
+    'https://jblanchardjr-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'
+]
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
