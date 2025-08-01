665c3ccf0cf383f99512c2c6d394b08be0fd650f
James Blanchard <jblanchardjr@gmail.com>
2025-08-01 03:39:10 -0400

    Completed CarMake/CarModel section: models, admin, populate file fixed, and get_car API working.

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -8,9 +8,12 @@ def initiate():
         {"name":"Kia", "description":"Great cars. Korean technology"},
         {"name":"Toyota", "description":"Great cars. Japanese technology"},
     ]
+
     car_make_instances = []
     for data in car_make_data:
             car_make_instances.append(CarMake.objects.create(name=data['name'], description=data['description']))
+
+
     # Create CarModel instances with the corresponding CarMake instances
     car_model_data = [
       {"name":"Pathfinder", "type":"SUV", "year": 2023, "car_make":car_make_instances[0]},
@@ -30,5 +33,6 @@ def initiate():
       {"name":"Kluger", "type":"SUV", "year": 2023, "car_make":car_make_instances[4]},
         # Add more CarModel instances as needed
     ]
+
     for data in car_model_data:
             CarModel.objects.create(name=data['name'], car_make=data['car_make'], type=data['type'], year=data['year'])
